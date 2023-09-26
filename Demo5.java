767b675336a5af7702f91ff701cada06da3c469a
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-26 16:26:52 +0530

    Next add

--- Demo5.java
+++ Demo5.java
@@ -1,5 +1,29 @@
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.nio.file.Path;
+
+
 public class Demo5 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
+        Path path = Paths.get("something.txt");
+       try( AsynchronousFileChannel channel = AsynchronousFileChannel.open(path, StandardOpenOption.READ)){
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        Future<Integer> read =channel.read(buffer, 0);
+        CompletableFuture<Void> completableFuture = new CompletableFuture<>();
+        completableFuture.runAsync(() ->{
+            
+
+        });
+        //System.out.println(read);
+        //lets do somthing here untill read the file 
+        System.out.println("i can execute this code ");
+       }
     }
 }
